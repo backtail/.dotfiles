b1386ef6fa49cfd308e186aa1e2915673440d6ba
Max Genson <accounts@maxgenson.de>
2025-11-09 21:49:41 +0100

    fix eval time clashing with musnix

--- nixconfig/shell/nixos-cli.nix
+++ nixconfig/shell/nixos-cli.nix
@@ -1,19 +1,23 @@
-{ config, system, pkgs, ...}:
+{
+  config,
+  system,
+  pkgs,
+  ...
+}:
 
 let
   # In pure evaluation mode, always use a full Git commit hash instead of a branch name.
   nixos-cli-url = "https://github.com/nix-community/nixos-cli/archive/refs/tags/0.14.0.tar.gz";
-  nixos-cli = import "${builtins.fetchTarball nixos-cli-url}" {inherit pkgs;};
-in {
+  nixos-cli = import "${builtins.fetchTarball nixos-cli-url}" { inherit pkgs; };
+in
+{
   imports = [
     nixos-cli.module
   ];
 
   services.nixos-cli = {
     enable = true;
-    config = {
-      # Other configuration for nixos-cli
-    };
+    prebuildOptionCache = false;
   };
 
   nix.settings = {
