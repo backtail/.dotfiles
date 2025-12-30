4ce1319d894e2f9fa71828114ce45471ea2ec4db
Max Genson <mail@maxgenson.de>
2025-12-30 12:48:57 +0100

    update qalc config version

--- nixconfig/os/backup.nix
+++ nixconfig/os/backup.nix
@@ -4,15 +4,15 @@
     kopia-ui
   ];
 
-  services.syncthing = {
-    enable = true;
-    extraFlags = [ "--no-default-folder" ];
-  };
+  # services.syncthing = {
+  #   enable = true;
+  #   extraFlags = [ "--no-default-folder" ];
+  # };
 
-  users.users.syncthing.extraGroups = [ "users" ];
-  users.users.gax.extraGroups = [ "syncthing" ];
-  systemd.services.syncthing.serviceConfig.UMask = "0007";
-  systemd.tmpfiles.rules = [
-    "d /home/gax 0750 gax syncthing"
-  ];
+  # users.users.syncthing.extraGroups = [ "users" ];
+  # users.users.gax.extraGroups = [ "syncthing" ];
+  # systemd.services.syncthing.serviceConfig.UMask = "0007";
+  # systemd.tmpfiles.rules = [
+  #   "d /home/gax 0750 gax syncthing"
+  # ];
 }
